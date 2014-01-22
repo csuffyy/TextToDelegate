d730c797a3daee95da8f9b76fa4ab936423afee9
csuffyy <csu-lzy@163.com>
2014-01-22 09:47:11 +0800

    初始版

---
+++ V1ForVS2008/src/Demo/FleeDemoForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Demo
+{
+    partial class FleeDemoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRed = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGreen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBlue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtRed
+            // 
+            this.txtRed.Location = new System.Drawing.Point(73, 77);
+            this.txtRed.Name = "txtRed";
+            this.txtRed.Size = new System.Drawing.Size(284, 21);
+            this.txtRed.TabIndex = 1;
+            this.txtRed.Text = "Math.Pow(x,2.0) % y";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Red:";
+            // 
+            // txtGreen
+            // 
+            this.txtGreen.Location = new System.Drawing.Point(73, 104);
+            this.txtGreen.Name = "txtGreen";
+            this.txtGreen.Size = new System.Drawing.Size(284, 21);
+            this.txtGreen.TabIndex = 2;
+            this.txtGreen.Text = "y % x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Green:";
+            // 
+            // txtBlue
+            // 
+            this.txtBlue.Location = new System.Drawing.Point(73, 131);
+            this.txtBlue.Name = "txtBlue";
+            this.txtBlue.Size = new System.Drawing.Size(284, 21);
+            this.txtBlue.TabIndex = 3;
+            this.txtBlue.Text = "x % y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Blue:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(394, 77);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(394, 129);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(83, 180);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "选择现有规则...."});
+            this.comboBox1.Location = new System.Drawing.Point(83, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(163, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // FleeDemoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 458);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBlue);
+            this.Controls.Add(this.txtGreen);
+            this.Controls.Add(this.txtRed);
+            this.Name = "FleeDemoForm";
+            this.Text = "FleeDemoForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGreen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBlue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ V1ForVS2008/src/Demo/FleeDemoForm.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class FleeDemoForm : Form
+    {
+        Timer _timer = new Timer();
+        int _index;
+        Func<double, double, int, Random, double> deleRed;
+        Func<double, double, int, Random, double> deleGreen;
+        Func<double, double, int, Random, double> deleBlue;
+        Rule[] rules = new Rule[]
+        {
+            new Rule(){
+                Name = "Blinds",
+                R = "Math.Round(4*x-y*2) % 2 - x",
+                G = "(Math.Abs(x+2*y) % 0.75)*10+y/5",
+                B = "Math.Round(Math.Sin(Math.Sqrt(x*x+y*y))*3/5)+x/3"
+            },
+            new Rule(){
+                Name = "Bullseye",
+                R = "1-Math.Round(x/y*0.5)",
+                G = "1-Math.Round(y/x*0.4)",
+                B = "Math.Round(Math.Sin(Math.Sqrt(x*x+y*y)*10))"
+            },
+            new Rule(){
+                Name = "Wave",
+                R = "Math.Cos(x/2)/2",
+                G = "Math.Cos(y/2)/3",
+                B = "Math.Round(Math.Sin(Math.Sqrt(x*x*x+y*y)*10))"
+            },
+            new Rule(){
+                Name = "Swirls",
+                R = "x*15",
+                G = "Math.Cos(x*y*4900)",
+                B = "y*15"
+            },
+            new Rule(){
+                Name = "Semi-Random",
+                R = "Math.Cos(x) * r.NextDouble()",
+                G = "Math.Pow(y,2.0)",
+                B = "Math.Pow(x,2.0)"
+            },
+            new Rule(){
+                Name = "Mod",
+                R = "Math.Pow(x,2.0) % y",
+                G = "y % x",
+                B = "x % y"
+            },
+            new Rule(){
+                Name = "Bullseye动画",
+                R = "1-Math.Round(x/y*0.5)",
+                G = "1-Math.Round(y/x*0.4)",
+                B = "Math.Round(Math.Sin(Math.Sqrt(x*x+y*y)*(t%50)))"
+            }
+        };
+
+        public FleeDemoForm()
+        {
+            InitializeComponent();
+
+            this.DoubleBuffered = true;
+
+            _timer.Tick += new EventHandler(_timer_Tick);
+            _timer.Interval = 40;
+
+            foreach (Rule rule in rules)
+            {
+                comboBox1.Items.Add(rule.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        void _timer_Tick(object sender, EventArgs e)
+        {
+            _index++;
+            GetImage();
+            //_timer.Stop();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            _index = 0;
+            deleRed = Zhucai.TextToDelegate.ExpressionParser.Compile<Func<double, double, int, Random, double>>("(x,y,t,r)=>" + txtRed.Text, "System");
+            deleGreen = Zhucai.TextToDelegate.ExpressionParser.Compile<Func<double, double, int, Random, double>>("(x,y,t,r)=>" + txtGreen.Text, "System");
+            deleBlue = Zhucai.TextToDelegate.ExpressionParser.Compile<Func<double, double, int, Random, double>>("(x,y,t,r)=>" + txtBlue.Text, "System");
+
+            _timer.Start();
+        }
+
+        Random rdm = new Random();
+        void GetImage()
+        {
+            const double mult = 2 * Math.PI / 256.0;
+
+            Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                double inputY = (y - 128) * mult;
+
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    double inputX = (x - 128) * mult;
+
+                    //Console.WriteLine("x:{0};y:{1}", inputX, inputY);
+
+                    double red;
+                    double green;
+                    double blue;
+
+                    int r, g, b;
+                    red = deleRed(inputX, inputY, _index, rdm);
+                    r = Process(red);
+
+                    green = deleGreen(inputX, inputY, _index, rdm);
+                    g = Process(green);
+
+                    blue = deleBlue(inputX, inputY, _index, rdm);
+                    b = Process(blue);
+
+                    bitmap.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            pictureBox1.Image = bitmap;
+        }
+
+        private int Process(double d)
+        {
+            if (d < 0 || double.IsNaN(d))
+            {
+                d = 0;
+            }
+            else if (d > 1)
+            {
+                d = 1;
+            }
+
+            return (int)(d * 255);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _timer.Stop();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex > 0)
+            {
+                Rule rule = rules[comboBox1.SelectedIndex - 1];
+                txtRed.Text = rule.R;
+                txtGreen.Text = rule.G;
+                txtBlue.Text = rule.B;
+            }
+        }
+    }
+
+    class Rule
+    {
+        public string Name { get; set; }
+        public string R { get; set; }
+        public string G { get; set; }
+        public string B { get; set; }
+    }
+}
